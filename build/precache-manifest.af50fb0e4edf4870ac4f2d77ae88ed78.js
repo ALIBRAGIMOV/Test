84c8ccf9d9a74f29b50cf212ea9972d6d51d354e
Ali <ialimagomed@mail.ru>
2020-08-17 04:39:04 +0300

    new

---
+++ build/precache-manifest.af50fb0e4edf4870ac4f2d77ae88ed78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52bc438ad211ab9415560e73971405c9",
+    "url": "/Test/index.html"
+  },
+  {
+    "revision": "0acfa62ff56e676de50b",
+    "url": "/Test/static/css/main.b34adc22.chunk.css"
+  },
+  {
+    "revision": "6fa8e37fdbe18a1e6ddc",
+    "url": "/Test/static/js/2.411ec174.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Test/static/js/2.411ec174.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0acfa62ff56e676de50b",
+    "url": "/Test/static/js/main.9bc031fa.chunk.js"
+  },
+  {
+    "revision": "bbad83194972342ac7a6",
+    "url": "/Test/static/js/runtime-main.2170a182.js"
+  }
+]);
