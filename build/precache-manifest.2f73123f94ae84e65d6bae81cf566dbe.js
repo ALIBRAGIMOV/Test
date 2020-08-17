857f834270aed13b6307550586371f8d2138437f
Ali <ialimagomed@mail.ru>
2020-08-17 03:54:56 +0300

    deploy two

---
+++ build/precache-manifest.2f73123f94ae84e65d6bae81cf566dbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25d1a921897e63f3804c7b9b12a717cf",
+    "url": "/TestRosEl/index.html"
+  },
+  {
+    "revision": "202f484fd8605b992a8e",
+    "url": "/TestRosEl/static/css/main.b34adc22.chunk.css"
+  },
+  {
+    "revision": "6fa8e37fdbe18a1e6ddc",
+    "url": "/TestRosEl/static/js/2.411ec174.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/TestRosEl/static/js/2.411ec174.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "202f484fd8605b992a8e",
+    "url": "/TestRosEl/static/js/main.f0136edc.chunk.js"
+  },
+  {
+    "revision": "308c43d974f3b615147f",
+    "url": "/TestRosEl/static/js/runtime-main.e461f6e2.js"
+  }
+]);
